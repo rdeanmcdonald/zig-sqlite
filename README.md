55dfb97ecb6de25037fe82525950b3f0c1a9171d
rmcdonald <richard_mcdonald@alumni.brown.edu>
2024-01-21 18:26:37 -0500

    scafold for statement parsing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Small sqlite clone in zig
+
+Following this series (written in c):
+https://cstack.github.io/db_tutorial/parts/part1.html
+
+Just learning zig and this article series seems to get into a lot of good lower
+level stuff. Not worrying about error handling or resource cleanup.
--- repl.zig
+++ repl.zig
@@ -5,6 +5,50 @@ const Allocator = mem.Allocator;
 const stdin = std.io.getStdIn().reader();
 const stdout = std.io.getStdOut().writer();
 
+const someerr = error{Err};
+const MetaCmdRes = enum { META_CMD_SUCCESS, META_CMD_UNRECOGNIZED };
+const StatementType = enum { STATEMENT_INSERT, STATEMENT_SELECT };
+const Statement = struct {
+    type: StatementType,
+
+    const Self = @This();
+
+    pub fn init(inbuf: *InputBuf) !Self {
+        if (inbuf.startsWith("insert")) {
+            return Self{
+                .type = StatementType.STATEMENT_INSERT,
+            };
+        }
+
+        if (inbuf.startsWith("select")) {
+            return Self{
+                .type = StatementType.STATEMENT_SELECT,
+            };
+        }
+
+        return someerr.Err;
+    }
+
+    pub fn exec(self: *Self) !void {
+        switch (self.type) {
+            StatementType.STATEMENT_INSERT => {
+                std.debug.print("Executing insert...\n", .{});
+            },
+            StatementType.STATEMENT_SELECT => {
+                std.debug.print("Executing select...\n", .{});
+            },
+        }
+    }
+};
+
+fn doMetaCmd(inbuf: *InputBuf) !void {
+    if (inbuf.startsWith(".exit")) {
+        std.os.exit(0);
+    } else {
+        return someerr.Err;
+    }
+}
+
 const InputBuf = struct {
     buf: []u8,
     inlen: usize,
@@ -15,7 +59,7 @@ const InputBuf = struct {
     pub fn init(allocator: *Allocator) !Self {
         const buflen = 4096;
         const buf = try allocator.alloc(u8, buflen);
-        return Self {
+        return Self{
             .buf = buf,
             .inlen = 0,
             .allocator = allocator,
@@ -29,28 +73,41 @@ const InputBuf = struct {
             std.debug.print("NO INPUT", .{});
         }
     }
-
-    pub fn input(self: *Self) []u8 {
+    pub fn getInput(self: *Self) []u8 {
         return self.buf[0..self.inlen];
+    }
+
+    pub fn startsWith(self: *Self, tst: []const u8) bool {
+        if (std.mem.startsWith(u8, self.getInput(), tst)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 
-fn print_prompt() void {
+fn printPrompt() void {
     std.debug.print("db > ", .{});
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
-    var inputbuf = try InputBuf.init(&allocator);
+    var inbuf = try InputBuf.init(&allocator);
     while (true) {
-        print_prompt();
-        try inputbuf.read();
-        if (std.mem.eql(u8, inputbuf.input(), ".exit")) {
-            std.debug.print("Exiting...\n", .{});
-            std.os.exit(0);
-        } else {
-            std.debug.print("Unrecognized command: {s}\n", .{inputbuf.input()});
+        printPrompt();
+        try inbuf.read();
+        if (inbuf.startsWith(".")) {
+            if (doMetaCmd(&inbuf)) |_| {} else |_| {
+                std.debug.print("Unrecognized meta command: {s}\n", .{inbuf.getInput()});
+            }
+            continue;
+        }
+        if (Statement.init(&inbuf)) |s| {
+            var statement = s;
+            try statement.exec();
+        } else |_| {
+            std.debug.print("Unrecognized statement: {s}\n", .{inbuf.getInput()});
         }
     }
 }
